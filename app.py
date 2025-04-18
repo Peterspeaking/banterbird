37dd13905b53f2806042ce3e50023fa0a5f3d2e3
topher2025 <topher.the.good.ninja@gmail.com>
2025-04-18 11:51:04 -0500

    Posts from backend

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+# Webpage
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
+
+
+
+# Other
+@app.route('/api/posts', methods=['GET'])
+def get_posts():
+    with open("posts.json", "r") as file:
+        posts = json.load(file)
+    return jsonify(posts)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
